75f50f6934f66682b75b75552b38836fdb0b735c
anubhav18sri-arch <anubhav18.sri@gmail.com>
2026-09-24 15:18:00 +0530

    Add: concatenation-of-array - submission-5

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-5.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+       result= nums+nums
+       return result
+       nums=  [1,4,1,2]
+       result= nums+nums
+       return result
